9df02173ddde69363af27a8a383deb78310ee091
302329760@student.rocmondriaan.nl <vijay99sankar@gmail.com>
2018-08-31 18:15:53 +0200

    Added sass as a default task in grunt. Also added the grunt command to the README file.

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,5 +17,5 @@ module.exports = grunt => {
 	});
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.registerTask('default',['watch']);
+	grunt.registerTask('default',['sass', 'watch']);
 }
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # team-custom
 
 ## setup
-### type `npm i` to install al the dependencies.
+* type `npm i` to install al the dependencies.
+* type `grunt` to watch the scss files it also creates the css folder
