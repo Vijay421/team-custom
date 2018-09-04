1a0efbf7fd35a9af97c9cf620c4e9921b50e99e7
302329760@student.rocmondriaan.nl <vijay99sankar@gmail.com>
2018-09-04 10:39:37 +0200

    added js to set the colours of the initials

--- js/index.js
+++ js/index.js
@@ -1,108 +1,12 @@
 window.onload = () => {
-	const columns = document.querySelectorAll('.payments-colunm');
-
-	for(const columnKey in database){
-		for(const object of database[columnKey]){
-				const article = document.createElement('ARTICLE');
-				article.classList.add('payment-detail');
-				
-				const innerHTML = filledInnerHTML(object);
-				article.innerHTML = innerHTML;
-
-				columns[columnKey].append(article);
-			}
-	}
+	setInitialColor();
 };
 
-function filledInnerHTML(object){
-	return `
-	<div class="payment-detail__body">
-		<p class="payment-detail__title">${object.title}</p>
-		<p class="payment-detail__discription">
-			${object.discription}
-		</p>
-	</div>
-	<p class="payment-detail__fee">${object.fee}</p>
-	<footer class="payment-detail__footer">
-		<div class="payment-detail__footer--left">
-			<p class="payment-detail__profile-initials">${object.initials}</p>
-			<p class="payment-detail__name">${object.name}</p>
-		</div>
-		<div class="payment-detail__footer--right">
-			<img class="payment-detail__calendar" src="calendar-regular.svg">
-			<p class="payment-detail__overdue-days">-${object.days} days overdue</p>
-		</div>
-	</footer>
-	`;
+function setInitialColor() {
+	const elements = document.querySelectorAll('.payment-detail__profile-initials');
+	for(const initials of elements){
+		initials.style.backgroundColor = initials.dataset.backcolor;
+		initials.style.color = initials.dataset.lettercolor; 
+		initials.style.borderColor = initials.dataset.backcolor;
+	}
 }
-
-const database = [
-	[
-		{
-			title: 'Practice Fee #1',
-			discription: 'Payment for preparation trainings for the Tournament.',
-			fee: '$10',
-			initials: 'SG',
-			name: 'Serge Gilroy',
-			days: '2',
-		},
-		{
-			title: 'Tournament #2',
-			discription: 'Payment for team participation in the tournament of the season.',
-			fee: '$130',
-			initials: 'MD',
-			name: 'Martin Devan',
-			days: '5',
-		},
-		{
-			title: 'Pratice #2',
-			discription: 'Payment for preparation trainings for the Tournament.',
-			fee: '$300',
-			initials: 'OC',
-			name: 'Olivia Courtney',
-			days: '10',
-		},	
-	],[
-		{
-			title: 'Equipment #1',
-			discription: 'Payment for providing full equipment necessary for training.',
-			fee: '$300',
-			initials: 'EL',
-			name: 'Emma Lindsie',
-			days: '20',
-		},
-		{
-			title: 'Practice Fee #1',
-			discription: 'Payment for preparation trainings for the Tournament.',
-			fee: '$240',
-			initials: 'TD',
-			name: 'Tom Dunno',
-			days: '24',
-		},
-	],[
-		{
-			title: 'Practice Fee #1',
-			discription: 'Payment for preparation tranings for the Tournament.',
-			fee: '$180',
-			initials: 'MR',
-			name: 'Matthew Ray',
-			days: '59',
-		},
-		{
-			title: 'Tournament #1',
-			discription: 'Payment for preparation tranings for the Tournament.',
-			fee: '$300',
-			initials: 'MR',
-			name: 'Micheal Ross',
-			days: '56',
-		},
-		{
-			title: 'Tournament #1',
-			discription: 'Payment for team participation in the tournament of the season.',
-			fee: '$130',
-			initials: 'MR',
-			name: 'Leon Quinian Ross',
-			days: '45',
-		},
-	]
-];
