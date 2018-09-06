3651ef5c254cd8e7a8317f1a3c5d1e5312caeae2
302329760@student.rocmondriaan.nl <vijay99sankar@gmail.com>
2018-09-06 16:09:18 +0200

    Edit var name.

--- js/index.js
+++ js/index.js
@@ -4,8 +4,8 @@ window.onload = () => {
 
 function setInitialColor() {
 	const elements = document.querySelectorAll('.payment-detail__border');
-	for(const initials of elements){
-		initials.childNodes[1].style.color = initials.childNodes[1].dataset.lettercolor;
-		initials.style.backgroundColor = initials.childNodes[1].dataset.backcolor;
+	for(const circle of elements){
+		circle.childNodes[1].style.color = circle.childNodes[1].dataset.lettercolor;
+		circle.style.backgroundColor = circle.childNodes[1].dataset.backcolor;
 	}
 }
