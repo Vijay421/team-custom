1f5c3eb8aa7dfac4504c44af603097910a0a99bf
302329760@student.rocmondriaan.nl <vijay99sankar@gmail.com>
2018-09-06 16:07:35 +0200

    removed console log

--- js/index.js
+++ js/index.js
@@ -5,7 +5,6 @@ window.onload = () => {
 function setInitialColor() {
 	const elements = document.querySelectorAll('.payment-detail__border');
 	for(const initials of elements){
-		console.log(initials.childNodes[1].dataset.backcolor);
 		initials.childNodes[1].style.color = initials.childNodes[1].dataset.lettercolor;
 		initials.style.backgroundColor = initials.childNodes[1].dataset.backcolor;
 	}
