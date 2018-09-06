f505eeb273869c9e95f1f9e696b73b69149b2bd4
unknown <brandonbetz@live.nl>
2018-09-06 10:17:36 +0200

    changed navbar item hover and the function in javascript

--- scss/partials/navbar.js
+++ scss/partials/navbar.js
@@ -1,10 +1,10 @@
 var list = document.getElementsByClassName("navbar");
 
-var items = document.getElementsByClassName("navbar__item");
+const items = document.getElementsByClassName("navbar__item");
 
-for (var i = 0;  i < items.length; i++) {
+for (const i in items) {
 	items[i].addEventListener("click", function() {
-		var current = document.getElementsByClassName("active");
+		const current = document.getElementsByClassName("active");
 		current[0].className = current[0].className.replace(" active", "");
 		this.className += " active";
 	});
